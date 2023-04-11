7f05ac2af703aa1c41132c275accdd3f160808cb
eunyoung <duddl4766@gmail.com>
2023-04-11 18:13:10 +0900

    update loginForm

--- project1/src/main/java/member/MemberDTO.java
+++ project1/src/main/java/member/MemberDTO.java
@@ -1,5 +1,77 @@
 package member;
 
+import java.sql.Timestamp;
+
 public class MemberDTO {
 
+	private String id;
+	private String pw;
+	private String name;
+	private String nickName;
+	private int birthday;
+	private String email;
+	private int pNum;
+	private Timestamp reg;
+	private String memberType;
+	private String press;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public int getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getpNum() {
+		return pNum;
+	}
+	public void setpNum(int pNum) {
+		this.pNum = pNum;
+	}
+	public Timestamp getReg() {
+		return reg;
+	}
+	public void setReg(Timestamp reg) {
+		this.reg = reg;
+	}
+	public String getMemberType() {
+		return memberType;
+	}
+	public void setMemberType(String memberType) {
+		this.memberType = memberType;
+	}
+	public String getPress() {
+		return press;
+	}
+	public void setPress(String press) {
+		this.press = press;
+	}
 }
