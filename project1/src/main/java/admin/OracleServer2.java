b82da68ad44bd765253374355b401dff8669930c
e-young <93626945+simeunyoung@users.noreply.github.com>
2023-04-21 14:21:56 +0900

    Add files via upload

---
+++ project1/src/main/java/admin/OracleServer2.java
@@ -0,0 +1,31 @@
+package admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class OracleServer2 {
+	protected Connection conn = null;
+	protected PreparedStatement pstmt = null;
+	protected ResultSet rs = null;
+	protected String sql = null;
+	
+	protected Connection getConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String user ="java01", pass = "java";
+			String dburl ="jdbc:oracle:thin:@nullmaster.sytes.net:1521:orcl";
+			conn = DriverManager.getConnection(dburl, user, pass);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	} // protected Connection getConnection() {
+	
+	protected void oracleClose() {
+		if(rs != null) {try {rs.close();} catch(Exception se) {}}
+		if(pstmt != null) {try {pstmt.close();} catch(Exception se) {}}
+		if(conn != null) {try {conn.close();} catch(Exception se) {}}
+	} // protected void oracleClose() {
+}
